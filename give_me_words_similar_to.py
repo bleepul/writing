f19a77674db0b801c10722aa0e1e674ec7a5a565
Brian Pulliam <bpulliam2010@gmail.com>
2019-08-12 11:02:03 -0700

    Added some comments to the thesarus

--- give_me_words_similar_to.py
+++ give_me_words_similar_to.py
@@ -1,24 +1,31 @@
 import spacy
 import argparse
 
+# initial arguments setup
 parser = argparse.ArgumentParser()
 parser.add_argument('-w', help='Word to lookup')
 parser.add_argument('-n', help='Number of similar words to return')
 args = parser.parse_args()
 
+
+# https://spacy.io/ ... probably need to update this regularly
 nlp = spacy.load('en_vectors_web_lg')
 
 
+# the basic function
 def most_similar(word, number):
     queries = [w for w in word.vocab if w.is_lower == word.is_lower]  # and w.prob >= -15]
     by_similarity = sorted(queries, key=lambda w: word.similarity(w), reverse=True)
     return [w.orth_ for w in by_similarity[:number]]
 
 
-#print([w.lower() for w in most_similar(nlp.vocab[args.w], int(args.n))])
+# print the output
 count = 1
 for w in most_similar(nlp.vocab[args.w], int(args.n)):
     print(w.lower())
     count = count + 1
     if count % 20 == 0:
         input()
+
+# alternative form
+# print([w.lower() for w in most_similar(nlp.vocab[args.w], int(args.n))])
